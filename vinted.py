032505db2ed53f7ae5b6747735f9d86b08d1548b
2spy <70370908+2spy@users.noreply.github.com>
2022-03-12 19:46:27 +0100

    V2 FIx

--- vinted.py
+++ vinted.py
@@ -1,10 +1,10 @@
 import os
 import shutil
-import json, asyncio
 try:
     import requests
     from bs4 import BeautifulSoup
     import discord
+    import json, asyncio
     import fade
     from discord_slash import ButtonStyle, SlashCommand
     from discord_slash.utils.manage_components import *
@@ -29,61 +29,66 @@ class Spy:
 
 
 def get_info_post(url):
-    reponse = requests.get(
-        str(url))
-    soup = BeautifulSoup(reponse.text, "html.parser")
+    try:
+        reponse = requests.get(
+            str(url))
+        soup = BeautifulSoup(reponse.text, "html.parser")
 
-    res = soup.findAll('script', {"class": "js-react-on-rails-component"})
+        res = soup.findAll('script', {"class": "js-react-on-rails-component"})
 
-    description = json.loads(res[15].text.replace(
-        '<script class="js-react-on-rails-component" data-component-name="ItemDescription" data-dom-id="ItemDescription-react-component-3d79657d-a1b5-4f1d-b501-2f470f328c66" type="application/json">',
-        "").replace("</script>", ''))
-    buybutton = json.loads(res[17].text.replace(
-        '<script class="js-react-on-rails-component" data-component-name="ItemBuyButton" data-dom-id="ItemBuyButton-react-component-026520bb-78fd-4e8f-9477-541dcafab42d" type="application/json">',
-        "").replace("</script>", ''))
-    userinfo = json.loads(res[19].text.replace(
-        '<script class="js-react-on-rails-component" data-component-name="ItemUserInfo" data-dom-id="ItemUserInfo-react-component-2105d904-b161-47d1-bfce-9b897a8c1cc6" type="application/json">',
-        '').replace("</script>", ''))
+        description = json.loads(res[15].text.replace(
+            '<script class="js-react-on-rails-component" data-component-name="ItemDescription" data-dom-id="ItemDescription-react-component-3d79657d-a1b5-4f1d-b501-2f470f328c66" type="application/json">',
+            "").replace("</script>", ''))
+        buybutton = json.loads(res[17].text.replace(
+            '<script class="js-react-on-rails-component" data-component-name="ItemBuyButton" data-dom-id="ItemBuyButton-react-component-026520bb-78fd-4e8f-9477-541dcafab42d" type="application/json">',
+            "").replace("</script>", ''))
+        userinfo = json.loads(res[19].text.replace(
+            '<script class="js-react-on-rails-component" data-component-name="ItemUserInfo" data-dom-id="ItemUserInfo-react-component-2105d904-b161-47d1-bfce-9b897a8c1cc6" type="application/json">',
+            '').replace("</script>", ''))
 
-    titre = description["content"]["title"]
-    description = description["content"]["description"]
-    buybutton = "https://www.vinted.fr" + buybutton["path"]
-    positive = userinfo["user"]["positive_feedback_count"]
-    negative = userinfo["user"]["negative_feedback_count"]
-    username = userinfo["user"]["login"]
-    pays = userinfo["user"]["country_title"]
-    ville = userinfo["user"]["city"]
+        titre = description["content"]["title"]
+        description = description["content"]["description"]
+        buybutton = "https://www.vinted.fr" + buybutton["path"]
+        positive = userinfo["user"]["positive_feedback_count"]
+        negative = userinfo["user"]["negative_feedback_count"]
+        username = userinfo["user"]["login"]
+        pays = userinfo["user"]["country_title"]
+        ville = userinfo["user"]["city"]
 
-    lesinfo = {}
-    lesinfo["titre"] = titre
-    lesinfo["description"] = description
-    lesinfo["buybutton"] = buybutton
-    lesinfo["positive"] = positive
-    lesinfo["negative"] = negative
-    lesinfo["username"] = username
-    lesinfo["pays"] = pays
-    lesinfo["ville"] = ville
-    return lesinfo
-
+        lesinfo = {}
+        lesinfo["titre"] = titre
+        lesinfo["description"] = description
+        lesinfo["buybutton"] = buybutton
+        lesinfo["positive"] = positive
+        lesinfo["negative"] = negative
+        lesinfo["username"] = username
+        lesinfo["pays"] = pays
+        lesinfo["ville"] = ville
+        return lesinfo
+    except:
+        pass
 
 def search(url):
-    reponse = requests.get(str(url))
-    soup = BeautifulSoup(reponse.text, "html.parser")
+    try:
+        reponse = requests.get(str(url))
+        soup = BeautifulSoup(reponse.text, "html.parser")
 
-    res = soup.findAll('script')
-    value = res[48].text.replace('<script z-js-react-on-rails-store="MainStore" type="application/json">', "")
-    z = json.loads(value)
+        res = soup.findAll('script')
+        value = res[48].text.replace('<script z-js-react-on-rails-store="MainStore" type="application/json">', "")
+        z = json.loads(value)
 
-    del z["intl"]
-    del z["session"]
-    del z["screen"]
-    del z["abTests"]
-    del z["auth"]
-    del z["savedSearches"]
-    del z["ads"]
-    del z["catalogFilters"]
-    del z["items"]["catalogItems"]["ids"]
-    return z
+        del z["intl"]
+        del z["session"]
+        del z["screen"]
+        del z["abTests"]
+        del z["auth"]
+        del z["savedSearches"]
+        del z["ads"]
+        del z["catalogFilters"]
+        del z["items"]["catalogItems"]["ids"]
+        return z
+    except:
+        pass
 
 with open("config.json", 'r') as config:
     configs = json.load(config)
@@ -122,36 +127,32 @@ async def moniteur(channelid, url):
     while True:
         try:
             z = search(str(url))
-        except Exception as err:
-            print(err)
-            pass
-
-        try:
+            await asyncio.sleep(1)
             x = z["items"]["catalogItems"]["byId"]
             dictlist = list(x)
             for i in range(7, -1, -1):
                 try:
                     post = dictlist[i]
 
-                    try:
-                        info = get_info_post(x[str(post)]["url"])
-                    except Exception as err:
-                        continue
-
+                    info = get_info_post(x[str(post)]["url"])
+                    await asyncio.sleep(1)
                     if post in posting:
                         continue
                     else:
                         embed = discord.Embed(title="``👕``" + " " + x[post]['title'],
                                               description=f"```fix\n{info['description']}```", url=x[post]['url'],
                                               color=0x09b7be)
-                        embed.add_field(name="**``💶`` Prix**", value=f"```yaml\n{x[post]['price']}€```", inline=True)
+                        embed.add_field(name="**``💶`` Prix**", value=f"```yaml\n{x[post]['price']}€```",
+                                        inline=True)
                         embed.add_field(name="**``📏`` Taille**", value=f"```yaml\n{x[post]['size_title']}```",
                                         inline=True)
                         embed.add_field(name="**``🔖`` Marque**", value=f"```yaml\n{x[post]['brand_title']}```",
                                         inline=True)
-                        embed.add_field(name="**``👍`` Avis positif**", value=f"```yaml\n{str(info['positive'])}```",
+                        embed.add_field(name="**``👍`` Avis positif**",
+                                        value=f"```yaml\n{str(info['positive'])}```",
                                         inline=True)
-                        embed.add_field(name="**``👎`` Avis négatif**", value=f"```yaml\n{str(info['negative'])}```",
+                        embed.add_field(name="**``👎`` Avis négatif**",
+                                        value=f"```yaml\n{str(info['negative'])}```",
                                         inline=True)
                         embed.add_field(name="**``📍`` Emplacement :**",
                                         value=f"```yaml\n{info['pays']}, {info['ville']}```", inline=True)
@@ -164,13 +165,18 @@ async def moniteur(channelid, url):
                                           url=str(x[str(post)]["url"])),
                         ]
                         action_row = create_actionrow(*buttons)
+                        await channel.send("<@437681180284813334>")
                         await channel.send(embed=embed, components=[action_row])
                         posting.append(post)
-                except:
-                    pass
-            await asyncio.sleep(5)
+                        await asyncio.sleep(1)
+                except Exception as err:
+                    print("L'erreur zebi : " + err)
+                    continue
 
-        except:
+            await asyncio.sleep(15)
+
+
+        except Exception as err:
             pass
 
 
@@ -216,16 +222,15 @@ async def run(ctx):
                 url="https://cdn.discordapp.com/avatars/939851736863105074/9a317142e664084d54c48a18ff46de40.webp?size=1024")
             embed.set_footer(text="つ ◕_◕ ༽つ Dev by 2$py#6495")
             await ctx.send(embed=embed)
-            await task
             break
-    await ctx.send(f"{ctx.author.mention} **- Aucun lien n'est affilié à ce salon !**")
-
+    await task
 
 
 @bot.command()
 async def stop(ctx, name):
     pending = asyncio.all_tasks()
     for ele in pending:
+        print(ele)
         if ele.get_name() == name:
             ele.cancel()
             await ctx.send(f"{ctx.author.mention} - **La tâche a été stoppé !**")
@@ -240,7 +245,7 @@ async def help(ctx):
     embed.set_thumbnail(
         url="https://cdn.discordapp.com/avatars/939851736863105074/9a317142e664084d54c48a18ff46de40.webp?size=1024")
     embed.add_field(name="$help", value="```yaml\nOuvre le menu d'aide du bot```", inline=False)
-    embed.add_field(name="$sub", value="```yaml\nLance le bot en fonction de l'url```", inline=False)
+    embed.add_field(name="$sub", value="```yaml\nLance le bot en fonction de l'url et de la limite```", inline=False)
     embed.add_field(name="$stop", value="```yaml\nEcrivez juste $stop nom_de_la_tache et le bot stoppera la tâche!```", inline=False)
     embed.add_field(name="$run", value="```yaml\nRelance la tâche associé au salon sans reécrire la commande $sub```", inline=False)
     embed.add_field(name="$ping", value="```yaml\nAffiche le ping du bot !```", inline=False)
